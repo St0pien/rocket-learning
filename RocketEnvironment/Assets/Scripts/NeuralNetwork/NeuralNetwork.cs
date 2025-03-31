55ff0987570aa23aa855d4055c4ac4dda400b9fc
avgmathenjoyer <avgmathenjoyer@gmail.com>
2025-03-31 23:35:29 +0200

    neural network

---
+++ RocketEnvironment/Assets/Scripts/NeuralNetwork/NeuralNetwork.cs
@@ -0,0 +1,123 @@
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
+using NEAT;
+using UnityEngine;
+using UnityEngine.Windows;
+using File = System.IO.File;
+
+public class NeuralNetwork
+{
+    // maybe those 2 could be switched out for array for better performance?
+    // currently leaving it out as dicts for simplicity
+    public Dictionary<int, NodeGene> Nodes;
+    public Dictionary<int, List<ConnectionGene>> Dependencies;
+    
+    public List<ConnectionGene> Connections;
+    public List<NodeGene> OutputNodes;
+    
+
+    public NeuralNetwork(List<NodeGene> nodes, List<ConnectionGene> connections)
+    {
+        OutputNodes = new List<NodeGene>();
+        Nodes = new Dictionary<int, NodeGene>();
+        foreach (var node in nodes)
+        {
+            Nodes.Add(node.Id, node);
+            if (node.Type == NodeType.Output)
+            {
+                OutputNodes.Add(node);
+            }
+        }
+        Connections = connections; // if we don't want connections with Status 0 then apply filter here
+        // construct dependencies list
+        Dependencies = new Dictionary<int, List<ConnectionGene>>();
+
+        foreach (var conn in Connections)
+        {
+            int endId = conn.Connection.Output;
+            if (Dependencies.ContainsKey(endId))
+            {
+                Dependencies[endId].Add(conn);
+            }
+            else
+            {
+                Dependencies.Add(endId, new List<ConnectionGene>(){conn});
+            }
+        }
+        
+    }
+
+    public Dictionary<int, float> CalculateValues(Dictionary<int, float> inputs)
+    {
+        var memo = new Dictionary<int, float>();
+        float Helper(int id)
+        {
+            if (memo.ContainsKey(id))
+            {
+                return memo[id];
+            }
+            if (!Dependencies.ContainsKey(id) || Dependencies[id].Count == 0)
+            {
+                if (Nodes[id].Type == NodeType.Sensor)
+                {
+                    return inputs[id];
+                }
+                // case when there is a hidden layer node not connected to any previous nodes...
+                // or not connected output
+                return 0;
+            }
+            float result = 0;
+            foreach (var dep in Dependencies[id])
+            {
+                int startNode = dep.Connection.Input;
+                result += dep.Weight * Helper(startNode);
+            }
+
+            memo.Add(id, result);
+            return result;
+        }
+        
+        var result = new Dictionary<int, float>();
+        foreach (var output in OutputNodes)
+        {
+            result[output.Id] = Helper(output.Id);
+        }
+
+        return result;
+    }
+}
+
+// read a population json file and provide util to get a particular neural network by given genome id
+public class PopulationJSONParser
+{
+    public List<Genome> Genomes;
+    public PopulationJSONParser(string filePath)
+    {
+        string helper = File.ReadAllText(filePath);
+        Genomes = JsonUtility.FromJson<Population>(helper).genomes;
+    }
+
+    public NeuralNetwork GetNeuralNetwork(int genomeId)
+    {
+        Genome found = Genomes.Find(x => x.Id == genomeId);
+
+        return new NeuralNetwork(found.NodeGenes, found.ConnectionGenes);
+    }
+}
+
+// read a file with singular Genome JSON representation
+public class GenomeJSONParser
+{
+    public Genome genome;
+
+    public GenomeJSONParser(string filePath)
+    {
+        string helper = File.ReadAllText(filePath);
+        genome = JsonUtility.FromJson<Genome>(helper);
+    }
+
+    public NeuralNetwork GetNeuralNetwork()
+    {
+        return new NeuralNetwork(genome.NodeGenes, genome.ConnectionGenes);
+    }
+}
